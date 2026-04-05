0dae426f7baa799a1b51fa16b3c8832fc80c67fb
Tubers93 <lprzemo16@gmail.com>
2026-04-05 12:15:06 +0200

    Create deploy.js

---
+++ deploy.js
@@ -0,0 +1,27 @@
+const { REST, Routes } = require('discord.js');
+const fs = require('node:fs');
+const path = require('node:path');
+
+const commands = [];
+const commandsPath = path.join(__dirname, 'commands');
+const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
+
+for (const file of commandFiles) {
+    const command = require(path.join(commandsPath, file));
+    commands.push(command.data.toJSON());
+}
+
+const rest = new REST({ version: '10' }).setToken(process.env.DISCORD_TOKEN);
+
+(async () => {
+    try {
+        console.log('Odświeżanie komend...');
+        await rest.put(
+            Routes.applicationCommands(process.env.CLIENT_ID),
+            { body: commands },
+        );
+        console.log('✅ Komendy zarejestrowane!');
+    } catch (error) {
+        console.error(error);
+    }
+})();
